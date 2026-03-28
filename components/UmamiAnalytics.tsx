2e1162d9e08e600f98df0b839a8348c1548659c7
yash18082002 <yash18082002@gmail.com>
2026-03-27 18:11:43 -0700

    improve analytics and update resume

--- components/UmamiAnalytics.tsx
+++ components/UmamiAnalytics.tsx
@@ -1,8 +1,16 @@
+"use client"
+
 import Script from "next/script";
 
 export const UmamiAnalytics = () => {
     const websiteId = process.env.NEXT_PUBLIC_UMAMI_WEBSITE_ID;
     const umamiUrl = process.env.NEXT_PUBLIC_UMAMI_URL;
+
+    const cleanUrl = () => {
+        if (window.location.search) {
+            window.history.replaceState({}, "", window.location.pathname);
+        }
+    }
 
     if (!websiteId || !umamiUrl) {
         return null;
@@ -13,6 +21,7 @@ export const UmamiAnalytics = () => {
             async
             src={`${umamiUrl}/script.js`}
             data-website-id={websiteId}
+            onLoad={cleanUrl}
         />
     )
 }
